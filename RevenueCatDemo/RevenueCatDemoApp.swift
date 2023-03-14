401906dbd1b61bea10474d1a2a2ea1d3e749e007
Logan Koshenka <koshenka27@gmail.com>
2023-03-14 12:56:12 -0400

    Initial Commit

---
+++ RevenueCatDemo/RevenueCatDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RevenueCatDemoApp.swift
+//  RevenueCatDemo
+//
+//  Created by Logan Koshenka on 3/14/23.
+//
+
+import SwiftUI
+
+@main
+struct RevenueCatDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
